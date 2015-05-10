e990c8d9c21727c33524f30f69a1f7adcd41018e
smunter6 <smunter6@gmail.com>
2015-05-10 15:58:13 -0500

    revision

---
+++ app/src/main/java/com/example/mobile/visualizer/VisualizerActivity.java
@@ -0,0 +1,67 @@
+package com.example.mobile.visualizer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class VisualizerActivity extends ServiceHandler {
+    private VisualizerView mVisualizerView;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualizer);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        // We need to link the visualizer view to the media player so that
+        // it displays something
+        mVisualizerView = (VisualizerView) findViewById(R.id.visualizerView);
+        mVisualizerView.link(musicSrv.getPlayer());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //menu item selected
+        switch (item.getItemId()) {
+            case R.id.action_shuffle:
+                musicSrv.setShuffle();
+                if(musicSrv.getShuffle()) {
+                    Toast.makeText(getApplicationContext(), "Shuffle On", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Shuffle Off", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.action_end:
+                stopService(playIntent);
+                musicSrv=null;
+                System.exit(0);
+                break;
+            case R.id.action_swap:
+                //unbindService(musicSrv);
+                //LocalBroadcastManager.getInstance(this).unregisterReceiver(onPrepareReceiver);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
